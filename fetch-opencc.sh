2850880de3e5d6b83917d93528b6073ee80dd67e
Gary W <garywill@disroot.org>
2021-11-03 12:09:51 +0000

    Update fetch-opencc.sh

--- fetch-opencc.sh
+++ fetch-opencc.sh
@@ -11,14 +11,15 @@ git checkout .
 git branch -a
 #ls -la
 #git log
-
+sha1sum opencc-data/TSCharacters.txt
 wget https://raw.githubusercontent.com/BYVoid/OpenCC/master/data/dictionary/TSCharacters.txt -O opencc-data/TSCharacters.txt
 
 
 git config user.name bot
 git config user.email bot@github.bot.none
 
+sha1sum opencc-data/TSCharacters.txt
 git add opencc-data/
-git git show
+git show
 git commit -m 'sync opencc'
 git log
