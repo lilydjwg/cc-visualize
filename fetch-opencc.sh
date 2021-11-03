1e798b8d36b3ab399d2e9cc9c2247661b8535e5a
garywill <garywill@disroot.org>
2021-11-03 19:57:21 +0800

    wget

--- fetch-opencc.sh
+++ fetch-opencc.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-uname -a
-pwd
-ls -la
-git branch -a
-git config --list
+#uname -a
+#pwd
+#ls -la
+#git branch -a
+#git config --list
 git fetch origin main
 git checkout main
 git checkout .
-ls -la
-git log
+git branch -a
+#ls -la
+#git log
 
+wget https://raw.githubusercontent.com/BYVoid/OpenCC/master/data/dictionary/TSCharacters.txt -O opencc-data/TSCharacters.txt
+
+
+git config user.name bot
+git config user.email bot@github.bot.none
+
+git add opencc-data/
+git git show
+git commit -m 'sync opencc'
+git log
